55e66197671d90d73e912de80399b5767ab00a0d
dodoromero88 <73591133+dodoromero88@users.noreply.github.com>
2021-02-26 14:45:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const MongoClient = require('mongodb').MongoClient();
 const app = express();
 
 // ========================
-var uri ="mongodb+srv://dbDodo:congo1988@cluster0.cffaf.mongodb.net/myDB?retryWrites=true&w=majority";
+var uri ="mongodb+srv://dbDodo:*****@cluster0.cffaf.mongodb.net/myDB?retryWrites=true&w=majority";
 // ========================
 
 
