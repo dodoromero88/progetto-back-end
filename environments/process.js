fb358f959e2ba7b542ba0116397a34930f939536
dodoromero88 <73591133+dodoromero88@users.noreply.github.com>
2021-03-08 15:40:28 +0100

    Delete directory environment

--- environments/process.js
+++
@@ -1,7 +0,0 @@
-export const process = {
-    production: false,
-    env: {
-      DB_URL: 'mongodb+srv://dbDodo:congo1988@cluster0.cffaf.mongodb.net/myDB?retryWrites=true&w=majority'
-      }
-    
-  };
